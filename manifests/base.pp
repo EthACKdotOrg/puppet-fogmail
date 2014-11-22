d19736eea26a9a6fcd1bd34123c210680f539af8
Cédric Jeanneret <tengu@ethack.org>
2014-11-22 21:05:53 +0100

    enforce order

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,15 +2,13 @@ class fogmail::base(
   $role,
 ) {
 
-  include ::fogmail::apt
+  class {'::fogmail::apt': }->
+  class {'::fogmail::debug': }->
 
-  if $::debug == 1 {
-    include ::fogmail::debug
-  }
-
+  class {'::sudo': }->
   sudo::conf {'vagrant-all':
     content => 'vagrant ALL=(ALL) NOPASSWD:ALL',
-  }
+  }->
 
   # SSH service
   class {'::ssh':
@@ -20,43 +18,41 @@ class fogmail::base(
       'PermitRootLogin'        => 'without-password',
       'X11Forwarding'          => 'no',
     },
-  }
+  }->
 
-  class {'::sudo': }
   class {'::ntp':
     restrict => ['127.0.0.1'],
-  }
+  }->
 
   # install cron/anacron in order to get some
   # periodic tasks, such as system updates, filter
   # updates for SA and so on
-  package {['cron', 'anacron']: }
+  package {['cron', 'anacron']: }->
 
   file {'/etc/profile.d/ps1.sh':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     content => template('fogmail/ps1.erb'),
-  }
+  }->
 
   class {'::xtreemfs::settings':
     add_repo => false,
     require  => Apt::Source['xtreemfs'],
-  }
+  }->
 
   file {"/etc/default/fogmail_${role}":
     ensure => file,
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
-  }
+  }->
 
   class {"::fogmail::role::${role}":
-    require => Class['fogmail::apt'],
-  }
+  }->
 
   package {'xtreemfs-tools':
-    require => Class['fogmail::apt'],
   }
+
 
 }
